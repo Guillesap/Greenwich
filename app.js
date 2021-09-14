60cf060a2001cd8f9f088e5a059e7d6e1512bfad
WillyS <hsapag@hotmail.com>
2021-09-13 23:52:11 -0300

    subiendo projecto js

---
+++ app.js
@@ -0,0 +1,9 @@
+alert ("Practicando JavaScript");
+let nombre= prompt("Ingresa tu nombre");
+let apellido= prompt("Ingresa tu apellido");
+let lenguaje= prompt("Ingresa tu lenguaje de programación preferido");
+
+console.log (nombre);
+console.log (apellido);
+console.log (lenguaje);
+
