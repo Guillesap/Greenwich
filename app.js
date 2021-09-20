35f3ea522cb706786439a5fe1c4f3acf6bf0bacf
WillyS <hsapag@hotmail.com>
2021-09-19 21:53:58 -0300

    [UPPDATE] practicando while

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// practicand alert 
 alert ("Bienvenido a la página de Greenwich Inglés");
 let nombre= prompt("Ingresa tu nombre");
 let apellido= prompt("Ingresa tu apellido");
@@ -33,4 +34,24 @@ if (edad >18){
     alert ("Puedes ingresar en curso Adolescentes/Adultos");
 }else {
     alert ("Puedes ingresar curso Niños")
+}
+
+// ciclos while
+
+let entrada = prompt ("Ingresa un nombre para estudiar inglés");
+while (entrada != "ESC" ) {
+    switch (entrada) {
+        case"maria":
+            alert ("Bienvenida a Greenwich Maria");
+            break;
+        case"guillermo":
+            alert ("Bienvenido a Greenwich Guillermo")
+            break;
+        default:
+            alert ("¿ No tenemos tu registro, quien sos?")
+            break;   
+    }
+    entrada = prompt ("Ingresa un nombre para estudiar inglés");
+            alert ("Gracias por visitar nuestro site")
+
 }
