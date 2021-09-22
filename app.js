b026e17672a9ae1df423141dd4ec323c8adb5d5e
WillyS <hsapag@hotmail.com>
2021-09-22 18:23:39 -0300

    [UPPDATE] Clase N° 4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 //DESAFIO CLASE N° 4
-var promedio = prompt ("Ingrese la nota de su último examen");
+var promedio = prompt ("Ingrese la nota de su último examen en Greenwich Inglés");
 if (promedio >10) {
     alert ("Error, tienes que ingresar una nota  del 1 al 10");
 }else {
@@ -13,8 +13,7 @@ if (promedio >10) {
 
 // OTRO EJEMPLO CLASE 4
 
-//por favor ingrese curso
-let curso = prompt("Por favor ingresá el curso que desee entre: niños, adolescentes, adultos y a distancia");
+let curso = prompt("Por favor ingresá el curso que desee digitando las palabras: niños, adolescentes, adultos y/o a distancia");
 switch (curso){
     case "niños":
         alert ("El curso para Niños está disponible, inicia el 01/10/2021");
@@ -26,22 +25,8 @@ switch (curso){
         alert ("El curso para Adultos está disponible, inicia el 05/10/2021");
         break;
     default:
-        alert ("El curso a distancia no está disponible actualmente. Se incorporará a la brevedad");
+        alert ("El curso a distancia no está disponible actualmente. Inicia el 01/12/2021");
         break;  
     }
 
-//COMPLEMENTARIO 4  CALCULAR IVA
-let numero = Number(prompt('Por favor ingrese el precio del libro de inglés de nuestro catálogo, que desea, sin IVA:'))
-let iva = Number(prompt('Por favor ingrese el número 21, siendo el mismo el porcentaje de IVA del producto:'))
-
-const calculadorIVA = (precio, porcentaje) => {
-    let precioConIva;
-    if(porcentaje < 10){
-        precioConIva =  precio * Number(`1.0${porcentaje}`)
-    }else{
-        precioConIva =  precio * Number(`1.${porcentaje}`)
-    }        
-    return `El porcentaje del Iva: ${porcentaje}, de su libro seleccionado, de $${precio}, resulta un total a abonar de  $${precioConIva}`;
-}
-alert(calculadorIVA(numero, iva))
 
