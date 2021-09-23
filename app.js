6f72287518eb0575cc28306fffb683a3ce80dfe4
WillyS <hsapag@hotmail.com>
2021-09-23 11:22:32 -0300

    [ADD] Tres funciones

--- app.js
+++ app.js
@@ -1,46 +1,5 @@
-
-//DESAFIO CLASE N° 4
-var promedio = prompt ("Ingrese la nota de su último examen en Greenwich Inglés");
-if (promedio >10) {
-    alert ("Error, tienes que ingresar una nota  del 1 al 10");
-}else {
-    if (promedio <6) {
-    alert ("Reprobaste, tienes que recuperar el módulo");
-    } else {
-    alert ("Felicitaciones, Aprobaste, continuamos en el próximo módulo");
-    }
-} 
-
-// OTRO EJEMPLO CLASE 4
-
-let curso = prompt("Por favor ingresá el curso que desee digitando las palabras: niños, adolescentes, adultos y/o a distancia");
-switch (curso){
-    case "niños":
-        alert ("El curso para Niños está disponible, inicia el 01/10/2021");
-        break;
-    case "adolescentes":
-        alert ("El curso para Adolescentes está disponible, inicia el 05/10/2021");
-        break;
-    case "adultos":
-        alert ("El curso para Adultos está disponible, inicia el 05/10/2021");
-        break;
-    default:
-        alert ("El curso a distancia no está disponible actualmente. Inicia el 01/12/2021");
-        break;  
-    }
-
-//OTRO EJEMPLO CLASE 4
-
-function imprimirPorconsola () {
-    let dato = prompt ("Ingrese 3 gustos para practicar inglés: Ejemplos: Escuchar música en inglés, ver Series, ver películas, etc")
-    let resultado = dato
-    console.log (resultado)
-}
-imprimirPorconsola()
-imprimirPorconsola()
-imprimirPorconsola()
-
-//COMPLEMENTARIO 4  CALCULAR IVA
+//COMPLEMENTARIO 4  
+//PRIMERA FUNCION  Calculador IVA
 let numero = Number(prompt('Por favor ingrese el precio del libro de inglés de nuestro catálogo, que desea, sin IVA:'))
 let iva = Number(prompt('Por favor ingrese el número 21, siendo el mismo el porcentaje de IVA del producto:'))
 
@@ -54,6 +13,50 @@ const calculadorIVA = (precio, porcentaje) => {
     return `El porcentaje del Iva: ${porcentaje}, de su libro seleccionado, de $${precio}, resulta un total a abonar de  $${precioConIva}`;
 }
 alert(calculadorIVA(numero, iva))
+
+//SEGUNDA FUNCION  Sumar precios
+let pedirPrecios = parseInt(prompt('Por favor ingresa el precio del producto que desea adquirir, al finalizar tipee una letra'))
+
+const sumarPrecios = (p) =>{
+    const precios = [p]
+    while(!isNaN(p)){
+        p = parseInt(prompt('ingresa precios, al termina escribe una letra'))
+        
+        if(isNaN(p)){
+            console.log(`Error`)
+        }else{
+            precios.push(p);
+        }
+    }
+
+    let suma = precios.reduce((a,b) => a+b,0)
+
+    
+    return suma;
+}
+alert(sumarPrecios(pedirPrecios))
+
+// TERCERA FUNCION  Calculador número multiplo
+let divisor = parseInt(prompt('Por favor ingresa un número '))
+let dividendo = parseInt(prompt('Por favor ingresa un segundo número'))
+
+while(isNaN(divisor)){
+    divisor = parseInt(prompt('ingresa un número valido'))
+}
+while(isNaN(dividendo)){
+    dividendo = parseInt(prompt('ingresa un número valido'))
+}
+
+const esMultiplo = (num1, num2) =>{
+    if(num1%num2 == 0){
+        return 'Es multiplo';
+    }else{
+        return 'No es multiplo';
+    } 
+}
+
+console.log(esMultiplo(divisor, dividendo))
+
 
 
 
