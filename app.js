065ce99e4703d42331678a1765a73829ee7d591e
WillyS <hsapag@hotmail.com>
2021-09-24 08:06:39 -0300

    [UPPDATE] Complementario 4

--- app.js
+++ app.js
@@ -1,63 +1,41 @@
 //COMPLEMENTARIO 4  
-//PRIMERA FUNCION  Calculador multiplo
-let divisor = parseInt(prompt('Por favor ingresa un número '))
-let dividendo = parseInt(prompt('Por favor ingresa un segundo número'))
 
-while(isNaN(divisor)){
-    divisor = parseInt(prompt('ingresa un número valido'))
-}
-while(isNaN(dividendo)){
-    dividendo = parseInt(prompt('ingresa un número valido'))
-}
+//FUNCIONES 
+solicitarDatos();
 
-const esMultiplo = (num1, num2) =>{
-    if(num1%num2 == 0){
-        return 'Es multiplo';
-    }else{
-        return 'No es multiplo';
-    } 
+// SOLICITUD PRECIOS DE LIBROS A USUARIO
+
+function solicitarDatos (){
+    let num1 = Number (prompt ("Por favor, ingrese precio de su libro o producto a adquirir"));
+    let num2 = Number (prompt ("Por favor, ingrese segundo precio de su libro o producto"));
+    let operador = prompt ("Por favor, para ver el monto final a abonar, ingrese el simbolo +");
+
+    calcular (num1, num2, operador);
 }
 
-console.log(esMultiplo(divisor, dividendo))
+// CALCULO
+function calcular (a,b,c) {
 
+    let res;
 
-//SEGUNDA FUNCION  Sumar precios
-let pedirPrecios = parseInt(prompt('Por favor ingresa el precio del producto que desea adquirir, al finalizar tipee una letra'))
+    switch (c) {
+        case "+": res= a+b;
+        break;
+        case "-": res= a-b;
+        break;
+        case "*": res= a*b;
+        break;
+        case "/": res= a/b;
+        break;
+        default: res= "La operación es incorrecta";
+        break;
+    }
+    resultado(res);
 
-const sumarPrecios = (p) =>{
-    const precios = [p]
-    while(!isNaN(p)){
-        p = parseInt(prompt('ingresa precios, al termina escribe una letra'))
-        
-        if(isNaN(p)){
-            console.log(`Error`)
-        }else{
-            precios.push(p);
-        }
+    //RESULTADO
+    function resultado (a){
+        console.log (a)
+        alert (a);
     }
 
-    let suma = precios.reduce((a,b) => a+b,0)
-
-    
-    return suma;
 }
-alert(sumarPrecios(pedirPrecios))
-
-// TERCERA FUNCION  CALCULAR IVA
-let numero = Number(prompt('Por favor ingrese el precio del libro de inglés de nuestro catálogo, que desea, sin IVA:'))
-let iva = Number(prompt('Por favor ingrese el número 21, siendo el mismo el porcentaje de IVA del producto:'))
-
-const calculadorIVA = (precio, porcentaje) => {
-    let precioConIva;
-    if(porcentaje < 10){
-        precioConIva =  precio * Number(`1.0${porcentaje}`)
-    }else{
-        precioConIva =  precio * Number(`1.${porcentaje}`)
-    }        
-    return `El porcentaje del Iva: ${porcentaje}, de su libro seleccionado, de $${precio}, resulta un total a abonar de  $${precioConIva}`;
-}
-alert(calculadorIVA(numero, iva))
-
-
-
-
