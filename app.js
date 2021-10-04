9f4ff70a1a4301a9e1315ac924191fbf10da4878
WillyS <hsapag@hotmail.com>
2021-10-04 01:18:20 -0300

    [ADD] Form

--- app.js
+++ app.js
@@ -1,41 +1,110 @@
-//COMPLEMENTARIO 4  
+/** Entidades */
 
-//FUNCIONES 
-solicitarDatos();
-
-// SOLICITUD PRECIOS DE LIBROS A USUARIO
-
-function solicitarDatos (){
-    let num1 = Number (prompt ("Por favor, ingrese precio de su libro o producto a adquirir"));
-    let num2 = Number (prompt ("Por favor, ingrese segundo precio de su libro o producto"));
-    let operador = prompt ("Por favor, para ver el monto final a abonar, ingrese el simbolo +");
-
-    calcular (num1, num2, operador);
-}
-
-// CALCULO
-function calcular (a,b,c) {
-
-    let res;
-
-    switch (c) {
-        case "+": res= a+b;
-        break;
-        case "-": res= a-b;
-        break;
-        case "*": res= a*b;
-        break;
-        case "/": res= a/b;
-        break;
-        default: res= "La operación es incorrecta";
-        break;
+class Person{
+    constructor(name, surname, email, phone, id){
+        return {
+            name: name,
+            surname : surname,
+            email : email,
+            phone : phone,
+            id: id
+        }
     }
-    resultado(res);
-
-    //RESULTADO
-    function resultado (a){
-        console.log (a)
-        alert (a);
-    }
-
-}
+  }
+  
+  /** Variables */
+  
+  /** Constantes */
+  
+  /** Selectores */
+  
+  const btn = document.querySelector('#guardar')
+  const table = document.querySelector('#table')
+  /** Arrays */
+  
+  let people = []
+  
+  /** Funciones */
+  
+  const guardar = () => {
+    const name = document.querySelector('#name').value
+    const surname = document.querySelector('#surname').value
+    const email = document.querySelector('#email').value
+    const phone = document.querySelector('#phone').value
+  
+    let listPeople = JSON.parse(localStorage.getItem('people'))
+  
+    if(localStorage.getItem('people') != null){
+      const id = uuidv4()
+      const person = new Person(name, surname, email, phone, id);
+      listPeople.push(person)
+      localStorage.setItem('people', JSON.stringify(listPeople))
+    }else{
+      localStorage.clear()
+      const id = uuidv4()
+      const person = new Person(name, surname, email, phone, id);
+      people.push(person)
+      localStorage.setItem('people', JSON.stringify(people))
+    }  
+    
+  }
+  
+  const print = () => {
+    const printer = JSON.parse(localStorage.getItem('people'))
+    printer.forEach(element => {
+      let tr = document.createElement('tr')
+      let td1 = document.createElement('td')
+      td1.setAttribute('class', 'col-1')
+      td1.textContent = printer.indexOf(element)
+      let th1 = document.createElement('th')
+      th1.setAttribute('class', 'col-2')
+      th1.textContent = element.name
+      let th2 = document.createElement('th')
+      th2.setAttribute('class', 'col-2')
+      th2.textContent = element.surname
+      let th3 = document.createElement('th')
+      th3.setAttribute('class', 'col-2')
+      th3.textContent = element.email
+      let th4 = document.createElement('th')
+      th4.setAttribute('class', 'col-2')
+      th4.textContent = element.phone
+      let td2 = document.createElement('td')
+      td2.setAttribute('class', 'col-1')
+  
+      let button = document.createElement("button")
+          button.setAttribute("class", "text-danger")
+          button.setAttribute("onclick", `deleteData(${element.id})`)
+          button.textContent = "X"
+  
+      tr.appendChild(td1);
+      tr.appendChild(th1);
+      tr.appendChild(th2);
+      tr.appendChild(th3);
+      tr.appendChild(th4);
+      td2.appendChild(button)
+      tr.appendChild(td2);
+  
+      table.appendChild(tr)
+    });
+  
+  }
+  
+  const deleteData = (id) => {
+    const peopleD = JSON.parse(localStorage.getItem('people'))
+    const update = peopleD.filter(person => person.id != id)
+    localStorage.setItem('people', JSON.stringify(update))
+    location.reload()
+  }
+  
+  const uuidv4 = () => {
+    return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+  
+  
+  /** Eventos */
+  btn.addEventListener('click', guardar)
+  /** Logica */
+  print()
